24ce95052b4920aac0b67ec91fe1f2e5a0dfb38d
ymir-gif <cabucosyamierzane@gmail.com>
2026-08-18 23:20:51 +0800

    tweak: S.A.F.E. starts at board level y=0, ends y=-300

--- src/js/animations.js
+++ src/js/animations.js
@@ -117,7 +117,7 @@ const slideAnimations = {
       animate(main, {
         innerHTML: scrambleText({ chars: 'A-Z0-9!@#$%' }),
         opacity: [0, 1],
-        y: [600, -100],
+        y: [0, -300],
         duration: 1400,
         ease: 'outExpo',
       });
